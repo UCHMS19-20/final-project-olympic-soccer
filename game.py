ee8f2b3bc5e716f8a349637661c49146bb4e62d8
EjikeI <58005802+EjikeI@users.noreply.github.com>
2020-01-10 13:21:09 -0500

    zeus_centered

--- game.py
+++ game.py
@@ -8,24 +8,16 @@ screen = pygame.display.set_mode( (width, height) )
 
 soccer_field = pygame.image.load("img/soccer_field.png")
 soccer_field = pygame.transform.scale(soccer_field, (400,600))
-screen.blit(soccer_field, (0,0))
-pygame.display.flip()
-#update
 
-soccer_ball = pygame.image.load("img/soccer_ball.png")
+soccer_ball = pygame.image.load("img/soccer_ball.png").convert_alpha()
 soccer_ball = pygame.transform.scale(soccer_ball, (50,50))
-screen.blit(soccer_ball, (width/2,height/2))
-pygame.display.flip()
 
-zeus = pygame.image.load("img/zeus_face.png")
+zeus = pygame.image.load("img/zeus_face.png").convert_alpha()
 zeus = pygame.transform.scale(zeus, (100,100))
-screen.blit(zeus, (width/2,height/4))
-pygame.display.flip()
 
-poseidon = pygame.image.load("img/poseidon_face.png")
+poseidon = pygame.image.load("img/poseidon_face.png").convert_alpha()
 poseidon = pygame.transform.scale(poseidon, (100,100))
-screen.blit(poseidon, (width/2,height/4))
-pygame.display.flip()
+
 while True:
     for evt in pygame.event.get():
         if evt.type == pygame.QUIT:
@@ -33,11 +25,12 @@ while True:
             sys.exit()
         elif evt.type == pygame.MOUSEMOTION:
             mouse_position = pygame.mouse.get_pos() # Where is the mouse at?
-            zx = mouse_position[0] # mouse_position is in the form [x,y], we only want the x part
-            zy = mouse_position[1]
+            zeus_rect = zeus.get_rect()
+            zeus_rect.center = mouse_position
             screen.blit(soccer_field, (0,0))
             screen.blit(soccer_ball, (width/2,height/2))
-            screen.blit(zeus, [zx, zy])
+            screen.blit(zeus, zeus_rect)
+            pygame.mouse.set_visible(False)
             screen.blit(poseidon, (width/2,height/4))
             pygame.display.flip()
         elif evt.type == pygame.MOUSEBUTTONDOWN:
