f53eb5dd16e33ef8ecf28505a26a354f753cf561
EjikeI <58005802+EjikeI@users.noreply.github.com>
2020-01-26 19:54:06 -0500

    Physics

--- game.py
+++ game.py
@@ -22,42 +22,53 @@ zeus = pygame.image.load("img/zeus_face.png").convert_alpha()
 zeus = pygame.transform.scale(zeus, (100,100))
 zeus_rect = zeus.get_rect()
 zeus_rect_pos = [width/2, height*3/4]
-zeus_rect_vel = [10,10]
+zeus_rect_vel = [2.5,2.5]
 
 poseidon = pygame.image.load("img/poseidon_face.png").convert_alpha()
 poseidon = pygame.transform.scale(poseidon, (100,100))
 poseidon_rect = poseidon.get_rect()
 poseidon_rect_pos = [width/2, height/4]
-poseidon_rect_vel = [10,10]
+poseidon_rect_vel = [2.5,2.5]
+
+pygame.key.set_repeat(10)
 
 while True:
-    if ball_rect.top == zeus_rect.bottom or ball_rect.top == poseidon_rect.bottom:
-        speed[1] = -40
-    if ball_rect.bottom == zeus_rect.top or ball_rect.bottom == poseidon_rect.top:
-        speed[1] = 40
-    if ball_rect.right == zeus_rect.left or ball_rect.right == poseidon_rect.left:
-        speed[0] = -40
-    if ball_rect.left == zeus_rect.right or ball_rect.left == poseidon_rect.right:
-        speed[0] = 40
+    
+    if speed[1] == 0:    
+        if ball_rect.left <= zeus_rect_pos[0] <= ball_rect.right or ball_rect.left <= poseidon_rect_pos[0] <= ball_rect.right:
+            if ball_rect.top <= zeus_rect.bottom or ball_rect.top <= poseidon_rect.bottom:
+                speed[1] = -40
+            if ball_rect.bottom >= zeus_rect.top or ball_rect.bottom >= poseidon_rect.top:
+                speed[1] = 40
+    if speed[0] == 0:
+        if ball_rect.top <= zeus_rect_pos[1] <= ball_rect.bottom or ball_rect.top <= poseidon_rect_pos[1] <= ball_rect.bottom:   
+            if ball_rect.right >= zeus_rect.left or ball_rect.right >= poseidon_rect.left:
+                speed[0] = -40
+            if ball_rect.left <= zeus_rect.right or ball_rect.left <= poseidon_rect.right:
+                speed[0] = 40
     
     ball_rect_pos[0] += speed[0]
     ball_rect_pos[1] += speed[1]
 
-# if statement for if the two things are close, the other things are applicable, put margins of error
+    if ball_rect.top <= zeus_rect_pos[1] <= ball_rect.bottom:
+        if ball_rect.left <= zeus_rect_pos[0] <= ball_rect.right:
+            speed[0] -= speed[0]
+    
+    if ball_rect.left <= zeus_rect_pos[0] <= ball_rect.right:
+        if ball_rect.top <= zeus_rect_pos[1] <= ball_rect.bottom:
+            speed[1] -= speed[1]
+    
+    if ball_rect.top <= poseidon_rect_pos[1] <= ball_rect.bottom:
+        if ball_rect.left <= poseidon_rect_pos[0] <= ball_rect.right:
+            speed[0] -= speed[0]
+    
+    if ball_rect.left <= poseidon_rect_pos[0] <= ball_rect.right:
+        if ball_rect.top <= poseidon_rect_pos[1] <= ball_rect.bottom:
+            speed[1] -= speed[1]
 
-    if ball_rect.right - 10 < zeus_rect.left < ball_rect.right + 10 or ball_rect.left < zeus_rect.right < ball_rect.left:
+    if ball_rect.left <= soccer_field_rect.left or ball_rect.right >= soccer_field_rect.right:
         speed[0] -= speed[0]
-    if ball_rect.bottom < zeus_rect.top < ball_rect.bottom or ball_rect.top < zeus_rect.bottom < ball_rect.top:
-        speed[1] -= speed[1]
-
-    if ball_rect.right < poseidon_rect.left < ball_rect.right or ball_rect.left < poseidon_rect.right < ball_rect.left:
-        speed[0] -= speed[0]
-    if ball_rect.bottom < poseidon_rect.top < ball_rect.bottom or ball_rect.top < poseidon_rect.bottom < ball_rect.top:
-        speed[1] -= speed[1]
-
-    if ball_rect.left or ball_rect.right == soccer_field_rect.left or soccer_field_rect.right:
-        speed[0] -= speed[0]
-    if ball_rect.top or ball_rect.bottom == soccer_field_rect.top or soccer_field_rect.bottom:
+    if ball_rect.top <= soccer_field_rect.top or ball_rect.bottom >= soccer_field_rect.bottom:
         speed[1] -= speed[1]
 
     for event in pygame.event.get():
@@ -85,6 +96,9 @@ while True:
             if event.key == pygame.K_d:
                 poseidon_rect_pos[0] += poseidon_rect_vel[0]
             
+            if event.key == pygame.K_SPACE:
+                print("Seize the seas!")
+
         elif event.type == pygame.MOUSEBUTTONDOWN:
             print("Lightning bolt!")
 
