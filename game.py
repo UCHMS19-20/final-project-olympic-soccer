9cb3b00342e7653b0de2d4089a95a2a279fd08c4
EjikeI <58005802+EjikeI@users.noreply.github.com>
2020-01-27 06:22:08 -0500

    Physics

--- game.py
+++ game.py
@@ -10,66 +10,59 @@ clock = pygame.time.Clock()
 
 soccer_field = pygame.image.load("img/soccer_field.png")
 soccer_field = pygame.transform.scale(soccer_field, (400,600))
-soccer_field_rect = soccer_field.get_rect()
 
 soccer_ball = pygame.image.load("img/soccer_ball.png").convert_alpha()
 soccer_ball = pygame.transform.scale(soccer_ball, (40,40))
-ball_rect = soccer_ball.get_rect()
-ball_rect_pos = [width/2, height/2]
+ball_pos = [width/2, height/2]
 speed = [0,0]
 
 zeus = pygame.image.load("img/zeus_face.png").convert_alpha()
 zeus = pygame.transform.scale(zeus, (100,100))
-zeus_rect = zeus.get_rect()
-zeus_rect_pos = [width/2, height*3/4]
-zeus_rect_vel = [2.5,2.5]
+zeus_pos = [width/2, height*3/4]
+zeus_vel = [2.5,2.5]
 
 poseidon = pygame.image.load("img/poseidon_face.png").convert_alpha()
 poseidon = pygame.transform.scale(poseidon, (100,100))
-poseidon_rect = poseidon.get_rect()
-poseidon_rect_pos = [width/2, height/4]
-poseidon_rect_vel = [2.5,2.5]
+poseidon_pos = [width/2, height/4]
+poseidon_vel = [2.5,2.5]
 
 pygame.key.set_repeat(10)
 
 while True:
-    
     if speed[1] == 0:    
-        if ball_rect.left <= zeus_rect_pos[0] <= ball_rect.right or ball_rect.left <= poseidon_rect_pos[0] <= ball_rect.right:
-            if ball_rect.top <= zeus_rect.bottom or ball_rect.top <= poseidon_rect.bottom:
-                speed[1] = -40
-            if ball_rect.bottom >= zeus_rect.top or ball_rect.bottom >= poseidon_rect.top:
-                speed[1] = 40
+        if ball_pos[0] <= zeus_pos[0] <= ball_pos[0] + 40 or ball_pos[0] <= poseidon_pos[0] <= ball_pos[0] + 40:
+            if ball_pos[1] <= zeus_pos[1] + 100 <= ball_pos[1] + 40 or ball_pos[1] <= poseidon_pos[1] + 100 <= ball_pos[1] + 40:
+                speed[1] = -5
+            if ball_pos[1] + 40 >= zeus_pos[1] >= ball_pos[1] or ball_pos[1] + 40 >= poseidon_pos[1] >= ball_pos[1]:
+                speed[1] = 5
     if speed[0] == 0:
-        if ball_rect.top <= zeus_rect_pos[1] <= ball_rect.bottom or ball_rect.top <= poseidon_rect_pos[1] <= ball_rect.bottom:   
-            if ball_rect.right >= zeus_rect.left or ball_rect.right >= poseidon_rect.left:
-                speed[0] = -40
-            if ball_rect.left <= zeus_rect.right or ball_rect.left <= poseidon_rect.right:
-                speed[0] = 40
-    
-    ball_rect_pos[0] += speed[0]
-    ball_rect_pos[1] += speed[1]
+        if ball_pos[1] <= zeus_pos[1] <= ball_pos[1] + 40 or ball_pos[1] <= poseidon_pos[1] <= ball_pos[1] + 40:   
+            if ball_pos[0] + 40 >= zeus_pos[0] >= ball_pos[0] or ball_pos[0] + 40 >= poseidon_pos[0] >= ball_pos[0]:
+                speed[0] = -5
+            if ball_pos[0] <= zeus_pos[0] + 100 <= ball_pos[0] + 40 or ball_pos[0] <= poseidon_pos[0] + 100 <= ball_pos[0] + 40:
+                speed[0] = 5
 
-    if ball_rect.top <= zeus_rect_pos[1] <= ball_rect.bottom:
-        if ball_rect.left <= zeus_rect_pos[0] <= ball_rect.right:
-            speed[0] -= speed[0]
+    if ball_pos[1] <= zeus_pos[1] <= ball_pos[1] + 40:
+        if ball_pos[0] <= zeus_pos[0] <= ball_pos[0] + 40:
+            speed[0] *= -1
+    if ball_pos[0] <= zeus_pos[0] <= ball_pos[0] + 40:
+        if ball_pos[1] <= zeus_pos[1] <= ball_pos[1] + 40:
+            speed[1] *= -1
     
-    if ball_rect.left <= zeus_rect_pos[0] <= ball_rect.right:
-        if ball_rect.top <= zeus_rect_pos[1] <= ball_rect.bottom:
-            speed[1] -= speed[1]
-    
-    if ball_rect.top <= poseidon_rect_pos[1] <= ball_rect.bottom:
-        if ball_rect.left <= poseidon_rect_pos[0] <= ball_rect.right:
-            speed[0] -= speed[0]
-    
-    if ball_rect.left <= poseidon_rect_pos[0] <= ball_rect.right:
-        if ball_rect.top <= poseidon_rect_pos[1] <= ball_rect.bottom:
-            speed[1] -= speed[1]
+    if ball_pos[1] <= poseidon_pos[1] <= ball_pos[1] + 40:
+        if ball_pos[0] <= poseidon_pos[0] <= ball_pos[0] + 40:
+            speed[0] *= -1
+    if ball_pos[0] <= poseidon_pos[0] <= ball_pos[0] + 40:
+        if ball_pos[1] <= poseidon_pos[1] <= ball_pos[1] + 40:
+            speed[1] *= -1
 
-    if ball_rect.left <= soccer_field_rect.left or ball_rect.right >= soccer_field_rect.right:
-        speed[0] -= speed[0]
-    if ball_rect.top <= soccer_field_rect.top or ball_rect.bottom >= soccer_field_rect.bottom:
-        speed[1] -= speed[1]
+    if ball_pos[0] <= 0 or ball_pos[0] + 40 >= 400:
+        speed[0] *= -1
+    if ball_pos[1] <= 0 or ball_pos[1] + 40 >= 600:
+        speed[1] *= -1
+
+    ball_pos[0] += speed[0]
+    ball_pos[1] += speed[1]
 
     for event in pygame.event.get():
         
@@ -79,22 +72,22 @@ while True:
        
         elif event.type == pygame.KEYDOWN: 
             if event.key == pygame.K_UP:
-                zeus_rect_pos[1] -= zeus_rect_vel[1]
+                zeus_pos[1] -= zeus_vel[1]
             if event.key == pygame.K_DOWN:
-                zeus_rect_pos[1] += zeus_rect_vel[1]
+                zeus_pos[1] += zeus_vel[1]
             if event.key == pygame.K_w:
-                poseidon_rect_pos[1] -= poseidon_rect_vel[1]
+                poseidon_pos[1] -= poseidon_vel[1]
             if event.key == pygame.K_s:
-                poseidon_rect_pos[1] += poseidon_rect_vel[1]
+                poseidon_pos[1] += poseidon_vel[1]
 
             if event.key == pygame.K_LEFT:
-                zeus_rect_pos[0] -= zeus_rect_vel[0]
+                zeus_pos[0] -= zeus_vel[0]
             if event.key == pygame.K_RIGHT:
-                zeus_rect_pos[0] += zeus_rect_vel[0]
+                zeus_pos[0] += zeus_vel[0]
             if event.key == pygame.K_a:
-                poseidon_rect_pos[0] -= poseidon_rect_vel[0]
+                poseidon_pos[0] -= poseidon_vel[0]
             if event.key == pygame.K_d:
-                poseidon_rect_pos[0] += poseidon_rect_vel[0]
+                poseidon_pos[0] += poseidon_vel[0]
             
             if event.key == pygame.K_SPACE:
                 print("Seize the seas!")
@@ -103,9 +96,9 @@ while True:
             print("Lightning bolt!")
 
     screen.blit(soccer_field, (0,0))
-    screen.blit(zeus, zeus_rect_pos)
-    screen.blit(poseidon, poseidon_rect_pos)
-    screen.blit(soccer_ball, ball_rect_pos)
+    screen.blit(zeus, zeus_pos)
+    screen.blit(poseidon, poseidon_pos)
+    screen.blit(soccer_ball, ball_pos)
     
     pygame.display.update()
     clock.tick(FPS)
