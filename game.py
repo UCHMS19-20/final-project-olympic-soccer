2402415e5f0fddcd79c83ce936aca06cf4193410
EjikeI <58005802+EjikeI@users.noreply.github.com>
2020-01-28 03:31:38 -0500

    Unfinished

--- game.py
+++ game.py
@@ -7,6 +7,7 @@ width, height = 400, 600
 screen = pygame.display.set_mode( (width, height) )
 FPS = 60
 clock = pygame.time.Clock()
+black = (0,0,0)
 
 soccer_field = pygame.image.load("img/soccer_field.png")
 soccer_field = pygame.transform.scale(soccer_field, (400,600))
@@ -29,65 +30,75 @@ poseidon_vel = [2.5,2.5]
 pygame.key.set_repeat(10)
 
 while True:
-    if speed[1] == 0:    
-        if ball_pos[0] <= zeus_pos[0] <= ball_pos[0] + 40 or ball_pos[0] <= poseidon_pos[0] <= ball_pos[0] + 40:
-            if ball_pos[1] <= zeus_pos[1] + 100 <= ball_pos[1] + 40 or ball_pos[1] <= poseidon_pos[1] + 100 <= ball_pos[1] + 40:
-                speed[1] = -5
-            if ball_pos[1] + 40 >= zeus_pos[1] >= ball_pos[1] or ball_pos[1] + 40 >= poseidon_pos[1] >= ball_pos[1]:
-                speed[1] = 5
-    if speed[0] == 0:
-        if ball_pos[1] <= zeus_pos[1] <= ball_pos[1] + 40 or ball_pos[1] <= poseidon_pos[1] <= ball_pos[1] + 40:   
-            if ball_pos[0] + 40 >= zeus_pos[0] >= ball_pos[0] or ball_pos[0] + 40 >= poseidon_pos[0] >= ball_pos[0]:
-                speed[0] = -5
-            if ball_pos[0] <= zeus_pos[0] + 100 <= ball_pos[0] + 40 or ball_pos[0] <= poseidon_pos[0] + 100 <= ball_pos[0] + 40:
-                speed[0] = 5
-
-    if ball_pos[1] <= zeus_pos[1] <= ball_pos[1] + 40:
-        if ball_pos[0] <= zeus_pos[0] <= ball_pos[0] + 40:
-            speed[0] *= -1
-    if ball_pos[0] <= zeus_pos[0] <= ball_pos[0] + 40:
-        if ball_pos[1] <= zeus_pos[1] <= ball_pos[1] + 40:
-            speed[1] *= -1
     
-    if ball_pos[1] <= poseidon_pos[1] <= ball_pos[1] + 40:
-        if ball_pos[0] <= poseidon_pos[0] <= ball_pos[0] + 40:
-            speed[0] *= -1
-    if ball_pos[0] <= poseidon_pos[0] <= ball_pos[0] + 40:
-        if ball_pos[1] <= poseidon_pos[1] <= ball_pos[1] + 40:
-            speed[1] *= -1
+    ball_left = ball_pos[0]
+    ball_right = ball_pos[0] + 40
+    ball_top = ball_pos[1]
+    ball_bottom = ball_pos[1] + 40
 
-    if ball_pos[0] <= 0 or ball_pos[0] + 40 >= 400:
+    zeus_left = zeus_pos[0]
+    zeus_right = zeus_pos[0] + 100
+    zeus_top = zeus_pos[1]
+    zeus_bottom = zeus_pos[1] + 100
+
+    poseidon_left = zeus_pos[0]
+    poseidon_right = zeus_pos[0] + 100
+    poseidon_top = zeus_pos[1]
+    poseidon_bottom = zeus_pos[1] + 100
+  
+    if zeus_left <= ball_left <= zeus_right or zeus_left <= ball_right <= zeus_right:
+        if zeus_top <= ball_bottom <= zeus_top + 40:
+            speed[1] = -5
+        if zeus_bottom - 40 <= ball_top <= zeus_bottom:
+            speed[1] = 5
+    if zeus_top <= ball_top <= zeus_bottom or zeus_top <= ball_bottom <= zeus_bottom:
+        if zeus_left <= ball_right <= zeus_left + 40:
+            speed[0] = -5
+        if zeus_right - 40 <= ball_left <= zeus_right:
+            speed[0] = 5
+    
+    if poseidon_left <= ball_left <= poseidon_right or poseidon_left <= ball_right <= poseidon_right:
+        if poseidon_top <= ball_bottom <= poseidon_top + 40:
+            speed[1] = -5
+        if poseidon_bottom - 40 <= ball_top <= poseidon_bottom:
+            speed[1] = 5
+    if poseidon_top <= ball_top <= poseidon_bottom or poseidon_top <= ball_bottom <= poseidon_bottom:
+        if poseidon_left <= ball_right <= poseidon_left + 40:
+            speed[0] = -5
+        if poseidon_right - 40 <= ball_left <= poseidon_right:
+            speed[0] = 5
+
+    if ball_left <= 0 or ball_right >= 400:
         speed[0] *= -1
-    if ball_pos[1] <= 0 or ball_pos[1] + 40 >= 600:
+    if ball_top <= 0 or ball_bottom >= 600:
         speed[1] *= -1
 
-    ball_pos[0] += speed[0]
-    ball_pos[1] += speed[1]
+    ball_left += speed[0]
+    ball_top += speed[1]
 
     for event in pygame.event.get():
-        
         if event.type == pygame.QUIT:
             pygame.quit()
             sys.exit()
        
         elif event.type == pygame.KEYDOWN: 
             if event.key == pygame.K_UP:
-                zeus_pos[1] -= zeus_vel[1]
+                zeus_top -= zeus_vel[1]
             if event.key == pygame.K_DOWN:
-                zeus_pos[1] += zeus_vel[1]
+                zeus_top += zeus_vel[1]
             if event.key == pygame.K_w:
-                poseidon_pos[1] -= poseidon_vel[1]
+                poseidon_top -= poseidon_vel[1]
             if event.key == pygame.K_s:
-                poseidon_pos[1] += poseidon_vel[1]
+                poseidon_top += poseidon_vel[1]
 
             if event.key == pygame.K_LEFT:
-                zeus_pos[0] -= zeus_vel[0]
+                zeus_left -= zeus_vel[0]
             if event.key == pygame.K_RIGHT:
-                zeus_pos[0] += zeus_vel[0]
+                zeus_left += zeus_vel[0]
             if event.key == pygame.K_a:
-                poseidon_pos[0] -= poseidon_vel[0]
+                poseidon_left -= poseidon_vel[0]
             if event.key == pygame.K_d:
-                poseidon_pos[0] += poseidon_vel[0]
+                poseidon_left += poseidon_vel[0]
             
             if event.key == pygame.K_SPACE:
                 print("Seize the seas!")
