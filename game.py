a09d99dde89a5f0ebdeb5a86d0e63dad6be93eeb
EjikeI <58005802+EjikeI@users.noreply.github.com>
2020-01-28 14:48:51 -0500

    Working

--- game.py
+++ game.py
@@ -1,124 +1,208 @@
 import sys
 import pygame
-
+# initializes pygame
 pygame.init()
-
+# sets the width and height of the screen
 width, height = 400, 600
 screen = pygame.display.set_mode( (width, height) )
+# sets the frames per second to 60
 FPS = 60
+# defines the pygame clock as a shorter variable
 clock = pygame.time.Clock()
-
+# creates a font
+font = pygame.font.SysFont('Calibri', 25, True, False)
+# defines the color white with RGB
+WHITE = (255, 255, 255)
+# the initial score of zeus
+score1 = 0
+# the initial score of poseidon
+score2 = 0
+# imports soccer field image and resizes it
 soccer_field = pygame.image.load("img/soccer_field.png")
 soccer_field = pygame.transform.scale(soccer_field, (400,600))
-
+# imports soccer ball image and resizes it
 soccer_ball = pygame.image.load("img/soccer_ball.png").convert_alpha()
 soccer_ball = pygame.transform.scale(soccer_ball, (40,40))
-ball_pos = [width/2, height/2]
+# sets the intial position of the ball
+ball_pos = [width/2 - 20, height/2 - 20]
+# sets the initial speed of the ball
 speed = [0,0]
-
+# imports image of zeus' face and resizes it
 zeus = pygame.image.load("img/zeus_face.png").convert_alpha()
 zeus = pygame.transform.scale(zeus, (100,100))
-zeus_pos = [width/2, height*3/4]
+# sets the initial position of zeus
+zeus_pos = [width/2 - 50, height*3/4 - 50]
+# sets the speed of zeus' movement
 zeus_vel = [2.5,2.5]
-
+# imports image of poseidon's face and resizes it
 poseidon = pygame.image.load("img/poseidon_face.png").convert_alpha()
 poseidon = pygame.transform.scale(poseidon, (100,100))
-poseidon_pos = [width/2, height/4]
+# sets the initial position of poseidon
+poseidon_pos = [width/2 - 50, height/4 - 50]
+# sets the speed of poseidon's movement
 poseidon_vel = [2.5,2.5]
-
+# allows the players to hold down keys and keep registering inputs
 pygame.key.set_repeat(10)
-
-while True: 
-    for event in pygame.event.get():
-        if event.type == pygame.QUIT:
-            pygame.quit()
-            sys.exit()
-       
-        elif event.type == pygame.KEYDOWN: 
-            if event.key == pygame.K_UP:
-                zeus_pos[1] -= zeus_vel[1]
-            if event.key == pygame.K_DOWN:
-                zeus_pos[1] += zeus_vel[1]
-            if event.key == pygame.K_w:
-                poseidon_pos[1] -= poseidon_vel[1]
-            if event.key == pygame.K_s:
-                poseidon_pos[1] += poseidon_vel[1]
-
-            if event.key == pygame.K_LEFT:
-                zeus_pos[0] -= zeus_vel[0]
-            if event.key == pygame.K_RIGHT:
-                zeus_pos[0] += zeus_vel[0]
-            if event.key == pygame.K_a:
-                poseidon_pos[0] -= poseidon_vel[0]
-            if event.key == pygame.K_d:
-                poseidon_pos[0] += poseidon_vel[0]
-            
-            if event.key == pygame.K_SPACE:
-                print("Seize the seas!")
-
-        elif event.type == pygame.MOUSEBUTTONDOWN:
-            print("Lightning bolt!")
-
+# continuous loop
+while True:
+    # defines the coordinates of the rectangle around the ball 
     ball_left = ball_pos[0]
     ball_right = ball_pos[0] + 40
     ball_top = ball_pos[1]
     ball_bottom = ball_pos[1] + 40
-
-    zeus_left = zeus_pos[0]
-    zeus_right = zeus_pos[0] + 100
-    zeus_top = zeus_pos[1]
-    zeus_bottom = zeus_pos[1] + 100
-
-    poseidon_left = poseidon_pos[0]
-    poseidon_right = poseidon_pos[0] + 100
-    poseidon_top = poseidon_pos[1]
-    poseidon_bottom = poseidon_pos[1] + 100
-
-    for x in range(ball_left, ball_right):
-        for y in range(zeus_left, zeus_right):
+    # defines the coordinates of the rectangle around zeus
+    zeus_left = zeus_pos[0] + 3.5
+    zeus_right = zeus_pos[0] + 96.5
+    zeus_top = zeus_pos[1] + 11
+    zeus_bottom = zeus_pos[1] + 89
+    # defines the coordinates of the rectangle around poseidon
+    poseidon_left = poseidon_pos[0] + 3.5
+    poseidon_right = poseidon_pos[0] + 96.5
+    poseidon_top = poseidon_pos[1] + 11
+    poseidon_bottom = poseidon_pos[1] + 89
+    # for multiple events pygame registers
+    for event in pygame.event.get():
+        # if the player quits, the code stops running
+        if event.type == pygame.QUIT:
+            pygame.quit()
+            sys.exit()
+        # if a key is pressed down
+        elif event.type == pygame.KEYDOWN: 
+            # if it is the up arrow key
+            if event.key == pygame.K_UP:
+                # zeus goes up once
+                zeus_pos[1] -= zeus_vel[1]
+            # if it is the down arrow key
+            if event.key == pygame.K_DOWN:
+                # zeus goes down once
+                zeus_pos[1] += zeus_vel[1]
+            # if it is the w key
+            if event.key == pygame.K_w:
+                # poseidon goes up once
+                poseidon_pos[1] -= poseidon_vel[1]
+            # if it is the s key
+            if event.key == pygame.K_s:
+                # poseidon goes down once
+                poseidon_pos[1] += poseidon_vel[1]
+            # if it is the left arrow key
+            if event.key == pygame.K_LEFT:
+                # zeus goes to the left once
+                zeus_pos[0] -= zeus_vel[0]
+            # if it is the right arrow key
+            if event.key == pygame.K_RIGHT:
+                # zeus goes to the right once
+                zeus_pos[0] += zeus_vel[0]
+            # if it is the a key
+            if event.key == pygame.K_a:
+                # poseidon goes to the left once
+                poseidon_pos[0] -= poseidon_vel[0]
+            # if it is the d key
+            if event.key == pygame.K_d:
+                # poseidon goes to the right once
+                poseidon_pos[0] += poseidon_vel[0]
+            # if it is the spacebar
+            if event.key == pygame.K_SPACE:
+                # it prints poseidon's taunt
+                print("Seize the seas!")
+        # if the mouse is clicked
+        elif event.type == pygame.MOUSEBUTTONDOWN:
+            # it prints zeus' taunt
+            print("Lightning bolt!")
+    # for the y values that the ball occupies
+    for x in range(int(ball_top), int(ball_bottom)):
+        # for the y values that zeus occupies
+        for y in range(int(zeus_top), int(zeus_bottom)):
+            # if they share a common y value
             if x == y:
-                if zeus_top in range(ball_bottom - 3, ball_bottom + 3):
-                    speed[1] = -5
-                if zeus_bottom in range(ball_top - 3, ball_top + 3):
-                    speed[1] = 5
-        for z in range(poseidon_left, poseidon_right):
+                # if the right of the ball touches the left of zeus
+                if int(ball_right) in range(int(zeus_left) - 3, int(zeus_left) + 3):
+                    # the ball goes to the left
+                    speed[0] = -5
+                # if the left of the ball touches the right of zeus
+                if int(ball_left) in range(int(zeus_right) - 3, int(zeus_right) + 3):
+                    # the ball goes to the right
+                    speed[0] = 5
+        # for the y values that poseidon occupies
+        for z in range(int(poseidon_top), int(poseidon_bottom)):
+            # if the ball and poseidon share a common y value
             if x == z:
-                if poseidon_top in range(ball_bottom - 3, ball_bottom + 3):
+                # if the right of the ball touches the left of poseidon
+                if int(ball_right) in range(int(poseidon_left) - 3, int(poseidon_left) + 3):
+                    # the ball goes to the left
+                    speed[0] = -5
+                # if the left of the ball touches the right of poseidon
+                if int(ball_left) in range(int(poseidon_right) - 3, int(poseidon_right) + 3):
+                    # the ball goes to the right
+                    speed[0] = 5
+    # for the x values that the ball occupies
+    for a in range(int(ball_left), int(ball_right)):
+        # for the x values that zeus occupies
+        for b in range(int(zeus_left), int(zeus_right)):
+            # if they share a common x value
+            if a == b:
+                # if the bottom of the ball touches the top of zeus
+                if int(ball_bottom) in range(int(zeus_top) - 3, int(zeus_top) + 3):
+                    # the ball goes up
                     speed[1] = -5
-                if zeus_bottom in range(ball_top - 3, ball_top + 3):
+                # if the top of the ball touches the bottom of zeus
+                if int(ball_top) in range(int(zeus_bottom) - 3, int(zeus_bottom) + 3):
+                    # the ball goes down
                     speed[1] = 5
-
-    for u in range(ball_top, ball_bottom):
-        for v in range(zeus_top, zeus_bottom):
-            if u == v:
-                if zeus_left in range(ball_right - 3, ball_right + 3):
-                    speed[0] = -5
-                if zeus_right in range(ball_left - 3, ball_left + 3):
-                    speed[0] = 5
-        for t in range(poseidon_top, poseidon_bottom):
-            if u == t:
-                if poseidon_left in range(ball_right, ball_right + 40):
-                    speed[0] = -5
-                if poseidon_right in range(ball_left - 40, ball_left):
-                    speed[0] = 5
-    
+        # for the x values that poseidon occupies
+        for c in range(int(poseidon_left), int(poseidon_right)):
+            # if the ball and poseidon share a common x value
+            if a == c:
+                # if the bottom of the ball touches the top of poseidon
+                if int(ball_bottom) in range(int(poseidon_top) - 3, int(poseidon_top) + 3):
+                    # the ball goes up
+                    speed[1] = -5
+                # if the top of the ball touches the bottom of poseidon
+                if int(ball_top) in range(int(poseidon_bottom) - 3, int(poseidon_bottom) + 3):
+                    # the ball goes down
+                    speed[1] = 5
+    # if the ball hits the left or right of the screen
+    if ball_left == 0 or ball_right == 400:
+        # it goes the opposite way
+        speed[0] *= -1
+    # if the ball hits the top or bottom of the screen
     if ball_top == 0 or ball_bottom == 600:
-        if ball_left >= 267 and ball_right <= 358:
+        # it goes in the opposite direction
+        speed[1] *= -1
+    # if the ball is in between the goal posts
+    if ball_left >= 133 and ball_right <= 267:
+        # if the ball is touching the top of the screen
+        if ball_top <= 0:
+            # zeus scores
+            score1 += 1
+            # the ball goes back to its initial position at rest
+            ball_pos = [width/2 - 20, height/2 - 20]
+            speed = [0,0]
+        # if the ball touches the bottom of the screen
+        if ball_bottom >= 600:
+            # poseidon scores
+            score2 += 1
+            # the ball goes back to its initial position at rest
             ball_pos = [width/2 - 20, height/2 - 20]
             speed = [0,0]
 
-    if ball_left <= 0 or ball_right >= 400:
-        speed[0] *= -1
-    if ball_top <= 0 or ball_bottom >= 600:
-        speed[1] *= -1
-
+# MAKE IT SO WHEN THE PLAYER SCORES AGAIN, THE FINAL ANIMATION AND TAKE OUT SPACES
+    
+    # for the ball, the speed is added to the position for each frame in order to get the new position
     ball_pos[0] += speed[0]
     ball_pos[1] += speed[1]
-
+    # the soccer field (background), zeus, poseidon, and the soccer ball are all loaded onto their respective positions
     screen.blit(soccer_field, (0,0))
     screen.blit(zeus, zeus_pos)
     screen.blit(poseidon, poseidon_pos)
     screen.blit(soccer_ball, ball_pos)
-    
+    # the font from before is used to make white text for zeus' score
+    text = font.render(str(score1), True, WHITE)
+    # the text is loaded onto a position on the screen (in between the upper goal posts)
+    screen.blit(text, [195, 8])
+    # the font from before is used to make white text for poseidon's score
+    text = font.render(str(score2), True, WHITE)
+    # the text is loaded onto a position on the screen (in between the lower goal posts)
+    screen.blit(text, [195, 571])
+    # the screen is refreshed at a rate of 60 frames per second
     pygame.display.update()
     clock.tick(FPS)
